10c4049b8e1f9fccb7b915c6b1184512f9e382ff
Leonardo Pedro <leonardodeoliveira.tds1.senai@gmail.com>
2024-08-15 09:05:52 -0300

    I added new exercises

--- Ex1.js
+++ Ex1.js
@@ -23,7 +23,7 @@ filme.diretores = "Neymar e Cristiano Ronaldo"
 
 console.log(filme);
 
-//exercício 3
+//Exercício 3
 
 const aluno = new Object();
 
@@ -32,3 +32,33 @@ aluno.turma = "2°C"
 aluno.idade = 16
 
 console.log(aluno);
+
+//Exercício 4
+
+const cachorro = new Object();
+
+cachorro.nome = "Pitoco"
+cachorro.raca = "Golden"
+cachorro.cor = "Amarelo"
+
+console.log(cachorro);
+
+//Exercício 5 
+
+const carro = new Object();
+
+carro.cor = "Prata"
+carro.modelo = "Mustang GT"
+carro.ano = 2013
+
+console.log(carro);
+
+//Exercício 6
+
+const produto = new Object();
+
+produto.descricao = "ContraBaixo"
+produto.marca = "Sire V7 Marcus Miller"
+produto.preco = 'R$ 4.500'
+
+console.log(produto);
