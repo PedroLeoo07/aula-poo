1c183536b60a3083dd28e975bf746d7973f120f1
Leonardo Pedro <leonardodeoliveira.tds1.senai@gmail.com>
2024-08-15 11:28:49 -0300

    Exercício completo

--- Exercícios Classes/Ex1.js
+++ Exercícios Classes/Ex1.js
@@ -4,10 +4,16 @@ class Pessoa {
         this.idade = idade;
         this.altura = altura;
     }
+    seApresentar(){
+        console.log(`Olá, meu nome é ${this.nome}, tenho ${this.idade} anos de idade e tenho ${this.altura} de altura, muito prazer em conhece-los :)`);
+        
+    }
 }
 
 const user = new Pessoa("Letícia", 14, 1.71);
 const user1 = new Pessoa("Leonardo", 16, 1.75);
 
-console.log(user);
-console.log(user1);
+user.seApresentar();
+
+//console.log(user);
+//console.log(user1);
